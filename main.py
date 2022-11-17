b7a4fd9076cb2a5e32308939e4154103392e4db7
Wanice <52546308+wanicedude@users.noreply.github.com>
2022-11-17 05:54:44 +0100

    Update main.py
    set the paddle movemt up and down using the screen.onkey method

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from turtle import Screen, Turtle
 
-tim = Turtle()
-tim.color("white")
-tim.shape("square")
-tim.penup()
-tim.shapesize(stretch_wid=5, stretch_len=5)
-tim.setpos(350, 0)
+paddle = Turtle()
+paddle.color("white")
+paddle.shape("square")
+paddle.penup()
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.goto(350, 0)
 
 
-# def g():
-#     tim.setheading(90)
-#     tim.forward(50)
-#
-#
-# def go():
-#     tim.setheading(270)
-#     tim.forward(50)
+def go_up():
+    new_y = paddle.ycor()+20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor()-20
+    paddle.goto(paddle.xcor(), new_y)
 
 
 screen = Screen()
@@ -23,6 +23,6 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.listen()
-# screen.onkey(g, "Up")
-# screen.onkey(go, "Down")
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 screen.exitonclick()
