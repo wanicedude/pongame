707a69dc1f28d2385c557f83db17bc481ec62936
Wanice <52546308+wanicedude@users.noreply.github.com>
2022-11-17 07:17:19 +0100

    Main Page
    Changed to OOP

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
-
-# Create Paddle
-paddle = Turtle()
-
-# set color and shape
-paddle.color("white")
-paddle.shape("square")
-
-# Remove paddle trail
-paddle.penup()
-
-# set paddle size. the default size os 20px by 20px/ We are setting this to 100px by 20px
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-
-# declare where the paddle should goto on the screen
-paddle.goto(350, 0)
 
 # create screen, its title, size and background colour
 screen = Screen()
@@ -26,23 +11,18 @@ screen.title("Pong")
 # screen animator removal
 screen.tracer(0)
 
-# define the go up and down function
+# Create new paddle object from the paddle class
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-def go_up():
-    new_y = paddle.ycor()+20
-    paddle.goto(paddle.xcor(), new_y)
 
-
-def go_down():
-    new_y = paddle.ycor()-20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-# listen to key press to control the movement of the paddle using the system button
+# # listen to key press to control the movement of the paddle using the system button
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 # to make things show on the screen
 game_is_on = True;
--- paddle.py
+++ paddle.py
@@ -1,36 +1,32 @@
 from turtle import Turtle
 
+class Paddle(Turtle): #paddle class is now the same as turtle
+    def __init__(self, position):
+        super().__init__() # allows paddle to inherit functionalities of turtle class
+        self.color("white")
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+      
+    
+    
+    
+    def go_up(self):
+        new_y = self.ycor()+20
+        self.goto(self.xcor(), new_y)
+    def go_down(self):
+        new_y = self.ycor()-20
+        self.goto(self.xcor(), new_y)
 
-class Paddle:
-    def __init__(self):
-        self.paddle = Turtle()
-        self.
 
 
-
-
-
-
-
-
-    # def __init__(self):
-    #     self.segments = []
-    #     self.create_snake()
-    #     self.head = self.segments[0]
-
-    # def create_snake(self):
-    #     for position in STARTING_POSITION:  # for position in pos:
-    #         self.add_segment(position)
-
-    # def add_segment(self, position):
-    #     new_segment = Turtle("square")
-    #     new_segment.color("white")
-    #     new_segment.penup()
-    #     new_segment.goto(position)  # segment_1.goto(pos)
-    #     self.segments.append(new_segment)
-
-    # def extend(self):
-    #     self.add_segment(self.segments[-1].position())
+        
+    
+    
+        
+        
+        
 
 
 
@@ -49,48 +45,3 @@ class Paddle:
 
 
 
-
-
-
-
-
-
-
-
-
-# # Create Paddle
-# paddle = Turtle()
-
-# # set color and shape
-# paddle.color("white")
-# paddle.shape("square")
-
-# # Remove paddle trail
-# paddle.penup()
-
-# # set paddle size. the default size os 20px by 20px/ We are setting this to 100px by 20px
-# paddle.shapesize(stretch_wid=5, stretch_len=1)
-
-# # declare where the paddle should goto on the screen
-# paddle.goto(350, 0)
-
-# # create screen, its title, size and background colour
-# screen = Screen()
-# screen.setup(width=800, height=600)
-# screen.bgcolor("black")
-# screen.title("Pong")
-
-# # screen animator removal
-# screen.tracer(0)
-
-# # define the go up and down function
-
-
-# def go_up():
-#     new_y = paddle.ycor()+20
-#     paddle.goto(paddle.xcor(), new_y)
-
-
-# def go_down():
-#     new_y = paddle.ycor()-20
-#     paddle.goto(paddle.xcor(), new_y)
